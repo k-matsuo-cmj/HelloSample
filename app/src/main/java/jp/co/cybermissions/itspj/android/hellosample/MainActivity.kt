cced8b67f2fb1974a5ac3406eb3ecbdc93a8bc2e
k.matsuo <k.matsuo@cybermissions.co.jp>
2020-09-05 11:24:23 +0900

    ANDROIDアプリの教科書・第4章 イベントとリスナ

---
+++ app/src/main/java/jp/co/cybermissions/itspj/android/hellosample/MainActivity.kt
@@ -0,0 +1,54 @@
+package jp.co.cybermissions.itspj.android.hellosample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 表示ボタンであるButtonオブジェクトを取得
+        val btClick = findViewById<Button>(R.id.btClick)
+        // リスナクラスのインスタンスを生成
+        val listner = HelloListner()
+        // 表示ボタンにリスナを設定
+        btClick.setOnClickListener(listner)
+
+        // クリアボタンであるButtonオブジェクトを取得
+        val btClear = findViewById<Button>(R.id.btClear)
+        // クリアボタンにリスナを設定
+        btClear.setOnClickListener(listner)
+    }
+
+    // ボタンをクリックしたときのリスナクラス
+    private inner class HelloListner : View.OnClickListener {
+        override fun onClick(view: View) {
+            // 名前入力欄であるEditTextオブジェクトを取得
+            val input = findViewById<EditText>(R.id.etName)
+            // メッセージを表示するTextViewオブジェクトを取得
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            when(view.id) {
+                // 表示ボタンの場合
+                R.id.btClick -> {
+                    // 入力された名前文字列を取得
+                    val inputStr = input.text.toString()
+                    // メッセージを表示
+                    output.text = inputStr + "さん、こんにちは！"
+                }
+                // クリアボタンの場合
+                R.id.btClear -> {
+                    // 名前入力欄に空文字を設定
+                    input.setText("")
+                    // メッセージ表示欄を空文字に設定
+                    output.text = ""
+                }
+            }
+        }
+    }
+}
